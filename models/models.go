b8dcc9a20c6fbd9a385715a3ba48f9fd5981b256
tiffany1618 <tiffany9342@gmail.com>
2020-04-27 16:05:13 -0700

    Added many to many relationships to models

--- models/models.go
+++ models/models.go
@@ -5,54 +5,49 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-// Location type definition
-type Location int
-
 // TODO - implement for privacy-forward implementation of this
 
 // UserLocation attempts to create a less obvious mapping of users to locations for privacy
 type UserLocation struct {
-	UUID         string   `json:"uuid"`
-	UserLocation Location `json:"location"`
+	UUID      string `json:"uuid"`
+	Latitude  float64
+	Longitude float64
 }
 
 // User user struct
 type User struct {
 	gorm.Model
-	FirstName   string  `json:"fname"`
-	LastName    string  `json:"lname"`
-	Email       string  `json:"username"`
-	Password    string  `json:"password"`
-	PhoneNumber string  `json:"phonenumber"`
-	ProfilePic  string  `json:"profilepic"`
-	UserID      string  `json:"userid"`
-	Major       string  `json:"degree"`
-	GradYear    string  `json:"year"`
-	Lat         float64 `json:"lat"`
-	Lon         float64 `json:"lon"`
-	Interests   string  `json:"interests"`
-	Clubs       string  `json:"clubs"`
-	Bio         string  `json:"bio"`
-}
-
-// Friends structure of friend
-type Friends struct {
-	gorm.Model
-	UserID       string `json:"uuid"`
-	FriendID     string `json:"fuuid"`
-	FReqMess     string `json:"msg"`
-	TimeStamp    int64  `json:"timestamp"`
-	Status       int    `json:"status"`
-	TimeAccepted int64  `json:"timeaccepted"`
+	FirstName   string `json:"fname"`
+	LastName    string `json:"lname"`
+	Email       string `json:"username"`
+	Password    string `json:"password"`
+	PhoneNumber string `json:"phonenumber"`
+	ProfilePic  string `json:"profilepic"`
+	Major       string `json:"degree"`
+	GradYear    string `json:"year"`
+	Location    UserLocation
+	Bio         string      `json:"bio"`
+	Clubs       []*Club     `gorm:"many2many:user_clubs;" json:"clubs"`
+	Interests   []*Interest `gorm:"many2many:user_interests;" json:"interests"`
+	Friends     []*User     `gorm:"many2many:user_friends;association_jointable_foreignkey:friend_id"`
 }
 
 // FriendRequest friend request
 type FriendRequest struct {
 	gorm.Model
-	Sender    string `json:"uuid"`
-	Receiver  string `json:"fuuid"`
-	Message   string `json:"message"`
-	Timestamp int64  `json:"timestamp"`
+	Receiver string `json:"fuuid"`
+	Message  string `json:"message"`
+}
+
+// Interest struct
+type Interest struct {
+	Interest string  `gorm:"primary_key" json:"interest"`
+	Users    []*User `gorm:"many2many:user_interests;"`
+}
+
+type Club struct {
+	Club  string
+	Users []*User `gorm:"many2many:user_clubs;"`
 }
 
 // Token tokens
@@ -84,12 +79,6 @@ type Message struct {
 	Sender   string `json:"sender"`
 	Username string `json:"username"`
 	Message  string `json:"message"`
-}
-
-// Interests interests
-type Interests struct {
-	UserID   string `json:"userid"`
-	Interest string `json:"interest"`
 }
 
 // Filterer filters
